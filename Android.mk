fe4caaa26a8f1df73d5b7c48c527ec8648e69385
roberto73c <roberto.cesina@phonemetra.com>
2015-03-27 17:53:24 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,3 +23,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
+		$(call all-renderscript-files-under, src) \
+		$(call all-proto-files-under, protos)
+		
+LOCAL_PROTOC_OPTIMIZE_TYPE := nano
+LOCAL_PROTOC_FLAGS := --proto_path=$(LOCAL_PATH)/protos/
+
+# LOCAL_SDK_VERSION := 21
+
+LOCAL_PACKAGE_NAME := Turbo
+LOCAL_PRIVILEGED_MODULE := true
+#LOCAL_CERTIFICATE := shared
